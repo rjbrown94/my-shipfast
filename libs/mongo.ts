a41d641bc3bf6ac967ce01e82aeb8454814f0541
Your Name <you@example.com>
2026-01-23 22:13:13 -0600

    fix mongo connection

--- libs/mongo.ts
+++ libs/mongo.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI!;
 
 if (!MONGODB_URI) {
-  throw new Error("Missing MONGODB_URI");
+  throw new Error("Missing MONGODB_URI in environment variables");
 }
 
 let cached = (global as any).mongoose;
